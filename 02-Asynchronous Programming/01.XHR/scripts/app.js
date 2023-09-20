3777cbdcd16f7a03b592e2382e964d36aa67c5f6
IlianCholpanov <iliancholpanov@gmail.com>
2023-09-20 18:43:39 +0300

    Update app.js

--- 02-Asynchronous Programming/01.XHR/scripts/app.js
+++ 02-Asynchronous Programming/01.XHR/scripts/app.js
@@ -1,3 +1,18 @@
 function loadRepos() {
-   console.log("TODO...");
+   let button = document.querySelector('button');
+   button.addEventListener('click', eventHandler);
+
+   function eventHandler() {
+      let url = 'https://api.github.com/users/testnakov/repos';
+      const httpRequest = new XMLHttpRequest();
+      httpRequest.addEventListener('readystatechange', eventHandler2);
+
+      function eventHandler2() {
+         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            document.getElementById("res").textContent = httpRequest.responseText;
+         }
+      }
+      httpRequest.open('GET', url);
+      httpRequest.send();
+   }
 }
