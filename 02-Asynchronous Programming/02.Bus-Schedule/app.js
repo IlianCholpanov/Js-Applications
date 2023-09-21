d499ebead285799e3c680236df658b481ef7a217
IlianCholpanov <iliancholpanov@gmail.com>
2023-09-21 15:08:52 +0300

    Update app.js

--- 02-Asynchronous Programming/02.Bus-Schedule/app.js
+++ 02-Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,17 +1,40 @@
 function solve() {
+    const label = document.querySelector("#info span");
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = {
+        next: 'depot'
+    };
+
+    async function depart() {
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+        const res = await fetch(url);
+
+        stop = await res.json();
+        departButton.disabled = true;
+        arriveButton.disabled = false;
+
+        label.textContent = `Next stop ${stop.name}`
+
+        if (res.status != 200) {
+            departButton.disabled = true;
+            arriveButton.disabled = true;
+            label.textContent = 'Error!'
+        }
     }
-
     function arrive() {
-        console.log('Arrive TODO...');
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+
+        label.textContent = `Arriving at ${stop.name}`
     }
 
     return {
         depart,
         arrive
-    };
+    }
 }
+
 
 let result = solve();
