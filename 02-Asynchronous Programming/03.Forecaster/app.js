38a7463eb773703a373ce32e4aa9d0f6aee2e03d
IlianCholpanov <iliancholpanov@gmail.com>
2023-09-21 16:26:47 +0300

    Update app.js

--- 02-Asynchronous Programming/03.Forecaster/app.js
+++ 02-Asynchronous Programming/03.Forecaster/app.js
@@ -3,43 +3,40 @@ function attachEvents() {
     const inputValue = document.getElementById('location');
     const forecast = document.getElementById('forecast');
     const symbols = {
-        Sunny: "&#x2600", // ☀
-        "Partly sunny": "&#x26C5", // ⛅
-        Overcast: "&#x2601", // ☁
+        Sunny: "&#x2600",
+        "Partly sunny": "&#x26C5",
+        Overcast: "&#x2601",
         Rain: "&#x2614",
-        Degrees: "&#176", // °
+        Degrees: "&#176",
     };
 
     getWeatherBtn.addEventListener('click', solve);
 
     function solve() {
         let url = `http://localhost:3030/jsonstore/forecaster/locations`;
-        
-        // Fetching locations
+
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                // Finding the index of the city in the locations data
                 let cityIndex = data.findIndex(el => el.name === inputValue.value);
                 if (cityIndex == -1) {
                     throw new Error('Invalid city');
                 }
                 let cityCode = data[cityIndex].code;
-                
+
                 forecast.style.display = 'block';
-                
-                // Fetching current conditions
+
                 fetch(`http://localhost:3030/jsonstore/forecaster/today/${cityCode}`)
                     .then(res => res.json())
                     .then(data => {
                         const currentForecast = document.querySelector('#current');
-                        
+
                         const conditonalSymbol = document.createElement('span');
                         conditonalSymbol.className = 'condition symbol';
                         conditonalSymbol.innerHTML = symbols[data.forecast.condition];
-                        currentForecast.innerHTML = ''; // Clear previous content
+                        currentForecast.innerHTML = '';
                         currentForecast.appendChild(conditonalSymbol);
-                        
+
                         const conditionList = document.createElement('span');
                         conditionList.className = 'condition';
                         conditionList.innerHTML = `
@@ -50,12 +47,11 @@ function attachEvents() {
                         currentForecast.appendChild(conditionList);
                     });
 
-                // Fetching 3-day forecast
                 fetch(`http://localhost:3030/jsonstore/forecaster/upcoming/${cityCode}`)
                     .then(res => res.json())
                     .then(data => {
                         const parentEl = document.getElementById('upcoming');
-                        parentEl.innerHTML = ''; // Clear previous content
+                        parentEl.innerHTML = '';
                         const foreCastInfo = document.createElement('div');
                         foreCastInfo.className = 'forecast-info';
 
